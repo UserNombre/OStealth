79cfb43117c0c077e5d952bd61ae515dc086d98d
UserNombre <pabloestebanbaquero@gmail.com>
2026-01-03 13:08:52 +0100

    ostealth.c: finish refactor and fix checksum issues

--- ostealth.c
+++ ostealth.c
@@ -20,7 +20,7 @@ struct os_config
     __u16 window_size;      // TCP Window Size
     __u8 ttl_value;         // Target TTL value
     __u8 df_flag;           // Don't Fragment flag
-    __u8 options_size;      // Size of TCP options
+    __u8 options_len;       // Length of TCP options
     __u8 options[40];       // TCP options
 };
 
@@ -32,19 +32,20 @@ struct
     __uint(max_entries, 1);
 } config_map SEC(".maps");
 
-static int __always_inline fetch_eth(struct __sk_buff *skb, struct ethhdr **eth)
+static int __always_inline fetch_ethhdr(struct __sk_buff *skb, struct ethhdr **eth)
 {
     *eth = (void *)(long)skb->data;
     return (void *)(*eth + 1) > (void *)(long)skb->data_end;
 }
 
-static int __always_inline fetch_ip(struct __sk_buff *skb, struct ethhdr *eth, struct iphdr **ip)
+static int __always_inline fetch_iphdr(struct __sk_buff *skb, struct iphdr **ip)
 {
+    struct ethhdr *eth = (void *)(long)skb->data;
     *ip = (void *)(eth + 1);
     return (void *)(*ip + 1) > (void *)(long)skb->data_end;
 }
 
-static int __always_inline fetch_tcp(struct __sk_buff *skb, struct iphdr *ip, struct tcphdr **tcp)
+static int __always_inline fetch_tcphdr(struct __sk_buff *skb, struct iphdr *ip, struct tcphdr **tcp)
 {
     *tcp = (void *)ip + ip->ihl * 4;
     return (void *)(*tcp + 1) > (void *)(long)skb->data_end;
@@ -60,22 +61,63 @@ static int __always_inline spoof_ip(struct __sk_buff *skb, struct iphdr *ip, str
         skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check),
         old_ttl, new_ttl, 2
     );
+    if(err < 0) return err;
 
     return err;
 }
 
 static int __always_inline spoof_tcp(struct __sk_buff *skb, struct iphdr *ip, struct tcphdr *tcp, struct os_config *cfg)
 {
-    __u16 old_window = tcp->window;
-    __u16 new_window = bpf_htons(cfg->window_size);
-    tcp->window = new_window;
+    long err;
+    __u32 old_buff, new_buff;
 
-    long err = bpf_l4_csum_replace(
-        skb, sizeof(struct ethhdr) + ip->ihl * 4 + offsetof(struct tcphdr, check),
-        old_window, new_window, 2
+    // Only update SYN (+ ACK) packets for now
+    if (!tcp->syn) return 0;
+
+    // Update IP fields
+    __u16 ip_hdr_len = ip->ihl * 4;
+    __u16 packet_len = sizeof(struct ethhdr) + ip_hdr_len + sizeof(struct tcphdr) + cfg->options_len;
+    __u16 old_tot_len = ip->tot_len;
+    __u16 new_tot_len = bpf_htons(packet_len - sizeof(struct ethhdr));
+    ip->tot_len = new_tot_len;
+
+    // Update TCP fields
+    tcp->window = bpf_htons(cfg->window_size);
+    __u16 old_tcp_len = bpf_htons(tcp->doff * 4);
+    __u16 new_tcp_len = bpf_htons(sizeof(struct tcphdr) + cfg->options_len);
+    tcp->doff = (sizeof(struct tcphdr) + cfg->options_len) / 4;
+
+    // Adjust packet length
+    err = bpf_skb_change_tail(skb, packet_len, 0);
+    if(err < 0) return err;
+
+    // Required verifier check
+    __u32 options_len = cfg->options_len;
+    if(options_len == 0 || options_len > 40)
+        return -1;
+
+    // Update TCP options
+    err = bpf_skb_store_bytes(
+        skb, sizeof(struct ethhdr) + ip_hdr_len + sizeof(struct tcphdr),
+        cfg->options, options_len, 0
     );
+    if(err < 0) return err;
 
-    return err;
+    // Recompute IP checksum
+    err = bpf_l3_csum_replace(
+        skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check),
+        old_tot_len, new_tot_len, 2
+    );
+    if(err < 0) return err;
+
+    // Recompute TCP checksum (only pseudo-header, since we assume offloading)
+    err = bpf_l4_csum_replace(
+        skb, sizeof(struct ethhdr) + ip_hdr_len + offsetof(struct tcphdr, check),
+        old_tcp_len, new_tcp_len, BPF_F_PSEUDO_HDR | 2
+    );
+    if(err < 0) return err;
+
+    return 0;
 }
 
 SEC("tc_egress")
@@ -91,29 +133,26 @@ int spoof_packet(struct __sk_buff *skb)
     struct tcphdr *tcp;
 
     // Fetch and validate the Ethernet header
-    if(fetch_eth(skb, &eth) != 0) return TC_ACT_SHOT;
+    if(fetch_ethhdr(skb, &eth) != 0) return TC_ACT_SHOT;
 
     // We only handle IPv4 for now
     if(eth->h_proto != bpf_htons(ETH_P_IP)) return TC_ACT_OK;
 
     // Fetch and validate the IP header
-    if(fetch_ip(skb, eth, &ip) != 0) return TC_ACT_SHOT;
+    if(fetch_iphdr(skb, &ip) != 0) return TC_ACT_SHOT;
 
     // Spoof fields in the IP header
     if(spoof_ip(skb, ip, cfg) < 0) return TC_ACT_SHOT;
 
     // Revalidate Ethernet and IP headers
-    if(fetch_eth(skb, &eth) != 0) return TC_ACT_SHOT;
-    if(fetch_ip(skb, eth, &ip) != 0) return TC_ACT_SHOT;
+    if(fetch_ethhdr(skb, &eth) != 0) return TC_ACT_SHOT;
+    if(fetch_iphdr(skb, &ip) != 0) return TC_ACT_SHOT;
 
     // The following code only applies to TCP packets
     if (ip->protocol != IPPROTO_TCP) return TC_ACT_OK;
 
     // Fetch and validate the TCP header
-    if(fetch_tcp(skb, ip, &tcp) != 0) return TC_ACT_SHOT;
-
-    // We only handle SYN packets for now
-    if (!tcp->syn || tcp->ack) return TC_ACT_OK;
+    if(fetch_tcphdr(skb, ip, &tcp) != 0) return TC_ACT_SHOT;
 
     // Spoof fields in the TCP header
     if(spoof_tcp(skb, ip, tcp, cfg) < 0)
